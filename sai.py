85652bbcb70f402f01fce6a67b31d127e7a31ceb
nagasaimalladi <45808997+nagasaimalladi@users.noreply.github.com>
2020-10-17 17:26:47 -0400

    chore: fix

--- sai.py
+++ sai.py
@@ -1,11 +1,11 @@
 import os
-file_type=".txt" # Make sure only .tt files are considered
+file_type=".sql" # Make sure only .tt files are considered
 word_count={}
 directory=input("Enter Folder Path:")
-os.chdir(directory)
+#os.chdir(directory)
 for filename in os.listdir(directory):
     if filename.endswith(file_type):
-        file_content = open(filename,"r")
+        file_content = open(directory+'/'+filename,"r")
         for line in file_content:
             words=line.split()
             for word in words:
@@ -14,4 +14,7 @@ for filename in os.listdir(directory):
                 else:
                     word_count[word.lower()]=1
         file_content.close()
-print({k: v for k, v in sorted(word_count.items(), key=lambda item: item[1],reverse=True) if v>=3})
+final = ({k: v for k, v in sorted(word_count.items(), key=lambda item: item[1],reverse=True) if v>=3})
+
+for l in final:
+    print(l, final[l])
